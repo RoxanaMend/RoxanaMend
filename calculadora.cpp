837c44bea9ac3028471f1e1090181396f1fa9c75
Roxy <roxana.mendoza.giron@utelvt.edu.ec>
2022-09-14 19:01:56 -0500

    estoy creando una calculadora

---
+++ calculadora.cpp
@@ -0,0 +1,17 @@
+# include < iostream>
+using namepace std;
+int main ()
+{
+	float n1,n2,s,r,m,d;
+	cout <<"ingrese el primer numero"; cin>>n1;
+	cout <<"ingrese el segundo numeeo"; cin>>n2;
+	s=n1+n2;
+	r=n1-n2;
+	m=n1*n2;
+	d=n1/n2;
+	cout <<"la suma de "<<n1<<"y de "<<n2<<"es igual a "<<s<<endl;
+	cout <<"la resta de "<<n1<<"-"<<n2<<"="<<endl;
+	cout <<"la multiplicacion de "<<n1<<"*"<<n2<<"="<<m<<endl;
+	cout <<"la divison de "<<n1<<"/"<<n2<<"="<<d<<endl;
+	return 0; 
+}
