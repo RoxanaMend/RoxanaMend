6a97a3650883d305097de167bdd93a138ca8cd67
Roxy <roxana.mendoza.giron@utelvt.edu.ec>
2022-09-20 14:36:00 -0500

    realizando la comparacion si es mayor

---
+++ compara.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float a,b;
+	cout<<"ingrese a b:";cin>>a>>b;
+	if(a>b)
+	{
+		cout<<a<<"es mayor a"<<b<<endl;
+	}else{
+		cout<<"es mayor a"<<a<<endl;
+	}
+	return 0; 
