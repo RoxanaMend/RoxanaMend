84d835413b56ce150f2d9c3027cc1918396417a0
RoxanaMend <112959961+RoxanaMend@users.noreply.github.com>
2022-09-27 11:41:06 -0500

    Update and rename RYMG_compara.cpp to compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float RYMG_a,RYMG_b;
+	cout<<"ingrese RYMG_a,RYMG_b:";cin>>RYMG_a>>RYMG_b;
+	if(RYMG_a>RYMG_b)
+	{
+		cout<<RYMG_a<<"es mayor RYMG_a"<<RYMG_b<<endl;
+	}else{
+		cout<<"es mayor RYMG_a"<<RYMG_a<<endl;
+	}
+	return 0; 
